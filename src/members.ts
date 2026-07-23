fc5fecbdefd41ee0ebb07598d629ca3d82fc6e1f
BloxForLife <ahmadamronawawi2014@gmail.com>
2026-07-23 18:25:20 +0300

    Add new member BloxForLife to members list

--- src/members.ts
+++ src/members.ts
@@ -107,7 +107,12 @@ export const MEMBERS: Member[] = [
   name: "nynele",
   buttonUrl: "https://raw.githubusercontent.com/Nynele/nynele/main/nugget.gif",
   discordId: "799251427839049818",                 
-  }
+  },
+  {
+  url: "https://bloxforlife.com",
+  name: "BloxForLife",
+  discordId: "1075335630353080340",                 
+  },
 ];
 
 export function getMemberByUrl(url: string): Member | undefined {
