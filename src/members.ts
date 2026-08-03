185c018073999e33348ee51bd79863a574f9d316
miliegoat <78791214+miliegoat@users.noreply.github.com>
2026-08-04 00:26:13 +0200

    updated buttons as my website repository is now private

--- src/members.ts
+++ src/members.ts
@@ -69,7 +69,7 @@ export const MEMBERS: Member[] = [
   {
     url: "https://chimera-realm.foo",
     name: "Chimera",
-    buttonUrl: "https://raw.githubusercontent.com/miliegoat/miliegoat.github.io/refs/heads/main/media/Untitled.gif",
+    buttonUrl: "https://raw.githubusercontent.com/miliegoat/miliegoat.github.io/refs/heads/main/media/untitled.gif",
     discordId: "736465046317563915",
   },
   {
